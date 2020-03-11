66be04c36b265d86a533f67da01f354c9b51f6a2
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2020-03-11 03:52:49 -0400

    Updated content

--- prepare_environment.sh
+++ prepare_environment.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+conda create --name cities2map
+conde activate cities2map
 conda install basemap
 conda install basemap-data-hires
 conda install -c conda-forge geopy
 conda install pandas
+conda install click
+conda install matplotlib
+conda deactivate
